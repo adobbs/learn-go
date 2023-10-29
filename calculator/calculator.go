069499148e7a63b2b1430ad52718cdc00a02bf59
Andy Dobbs <andrew.dobbs@gmail.com>
2023-10-29 22:29:29 +0000

    Finish simple calculator example

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func add(i int, j int) int { return i + j }
 
 func sub(i int, j int) int { return i - j }
@@ -8,6 +13,51 @@ func mul(i int, j int) int { return i * j }
 
 func div(i int, j int) int { return i / j }
 
-func main() {
+var opMap = map[string]func(int, int) int{
+	"+": add,
+	"-": sub,
+	"*": mul,
+	"/": div,
+}
 
+func main() {
+	expressions := [][]string{
+		{"2", "+", "3"},
+		{"2", "-", "3"},
+		{"2", "*", "3"},
+		{"2", "/", "3"},
+		{"2", "%", "3"},
+		{"two", "+", "three"},
+		{"5"},
+	}
+
+	for _, expression := range expressions {
+		// Check expression
+		if len(expression) != 3 {
+			fmt.Println("invalid expression:", expression)
+			continue
+		}
+		// Convert operand 1 to integer
+		p1, err := strconv.Atoi(expression[0])
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		// Use operator in expression to look up associated function
+		op := expression[1]
+		opFunc, ok := opMap[op]
+		if !ok {
+			fmt.Println("unsupported operator:", op)
+			continue
+		}
+		// Convert operand 2 to integer
+		p2, err := strconv.Atoi(expression[2])
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		// Call operation and print result
+		result := opFunc(p1, p2)
+		fmt.Println(result)
+	}
 }
